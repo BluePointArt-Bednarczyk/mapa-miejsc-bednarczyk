12d515be8183726125eefe74bfa709c6e2f28789
Blue Poit Art Gallery Project <53195000+jakub8745@users.noreply.github.com>
2024-11-06 19:06:19 +0000

    route added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { MapContainer, TileLayer, MapContainerProps, Marker, Popup, GeoJSON } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, GeoJSON } from 'react-leaflet';
 import L from 'leaflet';
 
 import 'leaflet/dist/leaflet.css'; // Import Leaflet CSS first
